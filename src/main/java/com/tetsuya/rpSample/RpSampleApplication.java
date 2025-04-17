63c7a0ea705f33027b01a1397b50324e52e1b93d
tetsuya <cinemamyg@gmail.com>
2025-04-17 03:31:52 -0400

    Initial commit

---
+++ src/main/java/com/tetsuya/rpSample/RpSampleApplication.java
@@ -0,0 +1,13 @@
+package com.tetsuya.rpSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tetsuya/rpSample/RpSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tetsuya.rpSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
