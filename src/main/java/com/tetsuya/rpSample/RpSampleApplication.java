69cbd64ad4a3c84ae56f8745d7c8d281026f43cb
tetsuya <cinemamyg@gmail.com>
2025-04-19 06:12:51 -0400

    test commit

--- src/main/java/com/tetsuya/rpSample/RpSampleApplication.java
+++ src/main/java/com/tetsuya/rpSample/RpSampleApplication.java
@@ -9,7 +9,9 @@ public class RpSampleApplication {
 	public static void main(String[] args) {
 
 		System.out.println("test");
+		System.out.println("test");
 		SpringApplication.run(RpSampleApplication.class, args);
+
 	}
 
 }
