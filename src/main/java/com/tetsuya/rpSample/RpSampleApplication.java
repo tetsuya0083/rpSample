ba773153c5cde662b048afefe90668881ea86a7e
tetsuya <cinemamyg@gmail.com>
2025-04-24 00:05:02 -0400

    log test

--- src/main/java/com/tetsuya/rpSample/RpSampleApplication.java
+++ src/main/java/com/tetsuya/rpSample/RpSampleApplication.java
@@ -8,7 +8,9 @@ public class RpSampleApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("test");
+		for (int i = 0; i < 10; i++){
+			System.out.println("test");
+		}
 		SpringApplication.run(RpSampleApplication.class, args);
 
 	}
