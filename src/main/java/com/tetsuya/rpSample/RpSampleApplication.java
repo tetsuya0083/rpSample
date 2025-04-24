a385a3a8a08e10ea9e40f5ffac808c5cca90f9c0
tetsuya <cinemamyg@gmail.com>
2025-04-24 00:20:29 -0400

    build test

--- src/main/java/com/tetsuya/rpSample/RpSampleApplication.java
+++ src/main/java/com/tetsuya/rpSample/RpSampleApplication.java
@@ -7,12 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RpSampleApplication {
 
 	public static void main(String[] args) {
-
-		for (int i = 0; i < 10; i++){
-			System.out.println("test");
-		}
-		SpringApplication.run(RpSampleApplication.class, args);
-
+		SpringApplication.run(RpSampleApplication.class, args)
 	}
 
 }
